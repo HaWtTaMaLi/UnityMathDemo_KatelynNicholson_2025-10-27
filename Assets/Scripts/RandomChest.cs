ba5ade7b1b30a335a21ed289aea552b0681b86f5
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-10-27 12:10:21 -0300

    Initial commit

---
+++ Assets/Scripts/RandomChest.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class RandomChest : MonoBehaviour
+{
+    //chest Type NAME SPACE
+    //wood chest
+    //bronze chest
+    //silver chest
+    //gold chest 
+    //platinum chest
+
+    //wood chest loot
+    //wood Common
+    //wood Uncommon
+    //wood rare 
+    //wood epic
+    //wood legendary
+
+    //Bronze chest loot
+    //bronze Common
+    //bronze Uncommon
+    //bronze rare 
+    //bronze epic
+    //bronze legendary
+
+    //Silver Chest Loot
+    //Silver Common
+    //silver Uncommon
+    //silver rare 
+    //silver epic
+    //silver legendary
+
+    //Gold chest loot
+    //Gold Common
+    //gold Uncommon
+    //gold rare 
+    //gold epic
+    //gold legendary
+
+    //platinum chest loot
+    //platinum Common
+    //platinum Uncommon
+    //platinum rare 
+    //platinum epic
+    //platinum legendary
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
