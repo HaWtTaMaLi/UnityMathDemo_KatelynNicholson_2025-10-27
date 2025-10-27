60fad91aa275e3470ccc44e1ac377cfaa069eafa
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-10-27 13:23:04 -0300

    drop table set up

--- Assets/Scripts/RandomChest.cs
+++ Assets/Scripts/RandomChest.cs
@@ -2,47 +2,47 @@ using UnityEngine;
 
 public class RandomChest : MonoBehaviour
 {
-    //chest Type NAME SPACE
-    //wood chest
-    //bronze chest
-    //silver chest
-    //gold chest 
-    //platinum chest
+    [Header("Chest Type")]
+    [SerializeField] private float WoodChest;
+    [SerializeField] private float BronzeChest;
+    [SerializeField] private float SilverChest;
+    [SerializeField] private float GoldChest;
+    [SerializeField] private float PlatinumChest; 
 
-    //wood chest loot
-    //wood Common
-    //wood Uncommon
-    //wood rare 
-    //wood epic
-    //wood legendary
+    [Header("Wood Chest Loot")]
+    [SerializeField] private float WoodChestCommon;
+    [SerializeField] private float WoodChestUncommon;
+    [SerializeField] private float WoodChestRare;
+    [SerializeField] private float WoodChestEpic;
+    [SerializeField] private float WoodChestLegendary;
 
-    //Bronze chest loot
-    //bronze Common
-    //bronze Uncommon
-    //bronze rare 
-    //bronze epic
-    //bronze legendary
+    [Header("Bronze Chest Loot")]
+    [SerializeField] private float BronzeChestCommon;
+    [SerializeField] private float BronzeChestUncommon;
+    [SerializeField] private float BronzeChestRare;
+    [SerializeField] private float BronzeChestEpic;
+    [SerializeField] private float BronzeChestLegendary;
 
-    //Silver Chest Loot
-    //Silver Common
-    //silver Uncommon
-    //silver rare 
-    //silver epic
-    //silver legendary
+    [Header("Silver Chest Loot")]
+    [SerializeField] private float SilverChestCommon;
+    [SerializeField] private float SilverChestUncommon;
+    [SerializeField] private float SilverChestRare;
+    [SerializeField] private float SilverChestEpic;
+    [SerializeField] private float SilverChestLegendary;
 
-    //Gold chest loot
-    //Gold Common
-    //gold Uncommon
-    //gold rare 
-    //gold epic
-    //gold legendary
+    [Header("Gold Chest Loot")]
+    [SerializeField] private float GoldChestCommon;
+    [SerializeField] private float GoldChestUncommon;
+    [SerializeField] private float GoldChestRare;
+    [SerializeField] private float GoldChestEpic;
+    [SerializeField] private float GoldChestLegendary;
 
-    //platinum chest loot
-    //platinum Common
-    //platinum Uncommon
-    //platinum rare 
-    //platinum epic
-    //platinum legendary
+    [Header("Platinum Chest Loot")]
+    [SerializeField] private float PlatinumChestCommon;
+    [SerializeField] private float PlatinumChestUncommon;
+    [SerializeField] private float PlatinumChestRare;
+    [SerializeField] private float PlatinumChestEpic;
+    [SerializeField] private float PlatinumChestLegendary;
 
     void Start()
     {
